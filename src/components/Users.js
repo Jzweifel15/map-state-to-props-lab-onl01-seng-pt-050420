d9a1eb0d4cc91b2f621d0b9a8e030517e4e9627b
Justin R. Zweifel <jzweifel15@yahoo.com>
2021-02-23 01:18:27 +0000

    Automatically backed up by Learn

--- src/components/Users.js
+++ src/components/Users.js
@@ -1,5 +1,9 @@
 import React, { Component } from 'react';
+<<<<<<< HEAD
 import { connect } from 'react-redux';
+=======
+import connect from "react-redux";
+>>>>>>> 0347e31cba5334a77228c366a19f96d083a8a3d5
 
 class Users extends Component {
 
@@ -10,7 +14,17 @@ class Users extends Component {
       <div>
         {this.props.numberOfUsers}
         <ul>
+<<<<<<< HEAD
           {users}
+=======
+          Users!
+          {
+            this.state.users.map(user => {
+              <li>{ user }</li>
+            })
+          }
+          <li>{ this.state.userCount }</li>
+>>>>>>> 0347e31cba5334a77228c366a19f96d083a8a3d5
         </ul>
       </div>
     )
@@ -20,8 +34,17 @@ class Users extends Component {
 const mapStateToProps = (state) => {
   return {
     users: state.users,
+<<<<<<< HEAD
     numberOfUsers: state.users.length
   };
 };
 
 export default connect(mapStateToProps)(Users)
+=======
+    userCount: state.userCount
+  }
+}
+
+// connect this component to Redux
+export default connect(mapStateToProps)(Users)
+>>>>>>> 0347e31cba5334a77228c366a19f96d083a8a3d5
--- src/index.js
+++ src/index.js
@@ -6,9 +6,13 @@ import App from './App'
 
 import manageUsers from './reducers/manageUsers'
 
+<<<<<<< HEAD
 let store = createStore(manageUsers,
   window.__REDUX_DEVTOOLS_EXTENSION__ &&
   window.__REDUX_DEVTOOLS_EXTENSION__());
+=======
+const store = createStore(manageUsers);
+>>>>>>> 0347e31cba5334a77228c366a19f96d083a8a3d5
 
 
 ReactDOM.render(
